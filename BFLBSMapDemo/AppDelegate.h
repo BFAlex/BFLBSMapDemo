ac1529a64109add308ffd9126d37d121d1f5d6fa
BFAlex <838318741@qq.com>
2016-08-16 16:48:30 +0800

    Initial Commit

---
+++ BFLBSMapDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BFLBSMapDemo
+//
+//  Created by Readboy_BFAlex on 16/8/16.
+//  Copyright © 2016年 BF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
